3f21137b8f8c4ab03ef7e0440404c453ff8c71ed
Andrés León <andres05leonfernandez@gmail.com>
2025-07-01 14:37:02 -0500

    primer commit

---
+++ ecolimpio/src/main/java/com/mycompany/ecolimpio/conexion/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecolimpio.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author andres
+ */
+public class Conexion {
+
+    private static Conexion instancia;
+    private Connection con;
+
+    private final String url = "jdbc:mysql://localhost:3306/Ecolimpio";
+    private final String user = "root";
+    private final String paswword = "root";
+
+    private Conexion() {
+        try {
+            con = DriverManager.getConnection(url, user, paswword);
+            System.out.println("✅ Conexión establecida.");
+        } catch (SQLException e) {
+            throw new RuntimeException("❌ Error al conectar a la base de datos: " + e.getMessage(), e);
+        }
+    }
+
+    public static Conexion getinstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+
+    }
+
+    public Connection getconexion() {
+        return con;
+    }
+
+}
---
+++ ecolimpio/src/main/java/com/mycompany/ecolimpio/conexion/testConexion.java
@@ -0,0 +1,20 @@
+package com.mycompany.ecolimpio.conexion;
+
+import java.sql.Connection;
+
+public class testConexion {
+    public static void main(String[] args) {
+        try {
+            Conexion conexion = Conexion.getinstancia();
+            Connection con = conexion.getconexion();
+
+            if (con != null && !con.isClosed()) {
+                System.out.println("✅ Conexión a la base de datos establecida correctamente.");
+            } else {
+                System.out.println("❌ La conexión está cerrada o es nula.");
+            }
+        } catch (Exception e) {
+            System.out.println("❌ Error al probar la conexión: " + e.getMessage());
+        }
+    }
+}
