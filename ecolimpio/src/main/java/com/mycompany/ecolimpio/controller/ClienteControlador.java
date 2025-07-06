859b2c94a8075012db6c00f7f4e78a43e3f82af8
Andres-leon-fernandez <andres05leonfernandez@gmail.com>
2025-07-06 14:39:51 -0500

    4

--- ecolimpio/src/main/java/com/mycompany/ecolimpio/controller/ClienteControlador.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.ecolimpio.controller;
-
-public class ClienteControlador {
-
-}
--- ecolimpio/src/main/java/com/mycompany/ecolimpio/vista/LoginFrame.java
+++ ecolimpio/src/main/java/com/mycompany/ecolimpio/vista/LoginFrame.java
@@ -8,12 +8,12 @@ package com.mycompany.ecolimpio.vista;
  *
  * @author Andres
  */
-public class login extends javax.swing.JFrame {
+public class LoginFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form login
+     * Creates new form LoginFrame
      */
-    public login() {
+    public LoginFrame() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new login().setVisible(true);
+                new LoginFrame().setVisible(true);
             }
         });
     }
